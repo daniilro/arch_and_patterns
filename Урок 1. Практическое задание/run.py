397ff4870f66625750873b6fb8802b481ada788b
daniilro <daniil_ro@mail.ru>
2021-04-23 11:56:58 +0300

    dev

--- Урок 1. Практическое задание/run.py
+++ Урок 1. Практическое задание/run.py
@@ -3,11 +3,12 @@
 '''
 
 from wsgiref.simple_server import make_server
-
-from main import application
+from simba_framework.main import Framework
+from urls import pc_list, fc_list
 
 #############################################################
-
+application = Framework(pc_list, fc_list)
+#############################################################
 PORT = 8002
 with make_server('', PORT, application) as httpd:
     print(f'Starting on port {PORT}...')
--- Урок 1. Практическое задание/views.py
+++ Урок 1. Практическое задание/views.py
@@ -1,1 +1,31 @@
-import os
+'''
+
+'''
+from simba_framework.templator import render
+
+
+#############################################################################
+class PcWelcome:
+    def __call__(self, request):
+        return '200 OK', [b'Welcome']
+
+
+#############################################################################
+class PcIndex:
+    def __call__(self, request):
+        return '200 OK', [render('index.html', data=request.get('data', None))]
+
+
+#############################################################################
+class PcAbout:
+    def __call__(self, request):
+        return '200 OK', [b'About']
+
+
+#############################################################################
+class PcInfo:
+    def __call__(self, request):
+        print(request)
+        return '200 OK', [render('info.html', timestamp=request.get('timestamp', None))]
+
+#############################################################################
