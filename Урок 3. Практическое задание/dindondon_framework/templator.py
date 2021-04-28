3010649fe2f03961fc88c9028508a0236fcfd1db
daniilro <daniil_ro@mail.ru>
2021-04-28 16:54:32 +0300

    dev

--- Урок 3. Практическое задание/dindondon_framework/templator.py
+++ Урок 3. Практическое задание/dindondon_framework/templator.py
@@ -2,16 +2,13 @@
 
 '''
 
-import os
-
-#from jinja2 import Template
+# from jinja2 import Template
 from jinja2 import FileSystemLoader
 from jinja2.environment import Environment
 
 
 #############################################################
 def render(template_name, folder='templates', **kwargs):
-
     env = Environment()
     env.loader = FileSystemLoader(folder)
 
--- Урок 3. Практическое задание/views.py
+++ Урок 3. Практическое задание/views.py
@@ -3,26 +3,34 @@
 '''
 from dindondon_framework.templator import render
 
+TEMPLATES_FOLDER = 'templates'
+
 
 #############################################################################
 class PcWelcome:
     def __call__(self, request):
         return '200 OK', [
             render(
-                'contact.html', data=request.get(
+                'contact.html', folder=TEMPLATES_FOLDER, data=request.get(
                     'data', None))]
 
 
 #############################################################################
 class PcIndex:
     def __call__(self, request):
-        return '200 OK', [render('index.html', data=request.get('data', None))]
+        return '200 OK', [
+            render(
+                'index.html', folder=TEMPLATES_FOLDER, data=request.get(
+                    'data', None))]
 
 
 #############################################################################
 class PcAbout:
     def __call__(self, request):
-        return '200 OK', [b'About']
+        return '200 OK', [
+            render(
+                'about.html', folder=TEMPLATES_FOLDER, data=request.get(
+                    'data', None))]
 
 
 #############################################################################
@@ -31,7 +39,7 @@ class PcInfo:
         print(request)
         return '200 OK', [
             render(
-                'info.html', timestamp=request.get(
+                'info.html', folder=TEMPLATES_FOLDER, timestamp=request.get(
                     'timestamp', None))]
 
 
@@ -40,7 +48,16 @@ class PcContact:
     def __call__(self, request):
         return '200 OK', [
             render(
-                'contact.html', data=request.get(
+                'contact.html', folder=TEMPLATES_FOLDER, data=request.get(
+                    'data', None))]
+
+
+#############################################################################
+class PcFeedback:
+    def __call__(self, request):
+        return '200 OK', [
+            render(
+                'feedback.html', folder=TEMPLATES_FOLDER, data=request.get(
                     'data', None))]
 
 
@@ -50,6 +67,7 @@ pc_list = {
     '/index/': PcIndex(),
     '/about/': PcAbout(),
     '/info/': PcInfo(),
+    '/feedback/': PcFeedback(),
 }
 
 #############################################################
