ed0199a49eba01a95fafbaca4f0f2935bdab0a7e
daniilro <daniil_ro@mail.ru>
2021-05-11 18:25:29 +0300

    dev

--- Урок 5. Практическое задание/run.py
+++ Урок 5. Практическое задание/run.py
@@ -7,15 +7,17 @@ from wsgiref.simple_server import make_server
 from dindondon_framework.main import *
 from urls import fc_list
 from views import pc_list
+import sys
 
 #############################################################
 
-# application = Framework(pc_list, fc_list)
+application = Framework(pc_list, fc_list)
 # application = DebugApplication(pc_list, fc_list)
-application = FakeApplication(pc_list, fc_list)
+#application = FakeApplication(pc_list, fc_list)
 
 #############################################################
 PORT = 8000
 with make_server('', PORT, application) as httpd:
+    print("\nInfo:\nUser Current Version:-", sys.version, "\n")
     print(f'Starting on port {PORT}...')
     httpd.serve_forever()
